240b93eea98788d3e6a6d470ba99ceb0b02430ae
artp <dky7034@naver.com>
2026-01-06 15:02:23 +0900

    fix: 회원가입 페이지 라우팅 주석 처리

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from "react";
-// import { Link, useNavigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import {
